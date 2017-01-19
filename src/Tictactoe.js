d81daba1c666855e3e9581caedb22f1225c9b83b
benjaminboruff <ben@boruff.me>
2017-01-19 09:21:49 -0500

    basic structure

---
+++ src/Tictactoe.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Tictactoe.css';
+
+class Tictactoe extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default Tictactoe;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Tictactoe from './Tictactoe';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Tictactoe />,
   document.getElementById('root')
 );
